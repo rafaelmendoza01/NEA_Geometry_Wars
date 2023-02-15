d0845c002fc5c5a7c2fd54f27d306633fbd72fde
SnakeyKing75 <rafantmendoza@gmail.com>
2023-02-15 11:12:10 +0000

    Started implementing Moves for escaping enemy

--- NEA_GeometryWars/Assets/Scripts/EscapeEnemy.cs
+++ NEA_GeometryWars/Assets/Scripts/EscapeEnemy.cs
@@ -6,7 +6,25 @@ using UnityEngine;
 public class EscapeEnemy : EnemyMovement
 {
     GameObject[] PlayerBullets; 
-
+    IDictionary<GameObject, float> ClosestBullets = new Dictionary<GameObject, float>();
+    
+    static void MergeSort(IDictionary<GameObject, float> Dictionary)
+    {
+        if (Dictionary.Count < 2)
+        {
+            return;
+        }
+        else
+        {
+            int midpoint = Dictionary.Count / 2;
+            IDictionary<GameObject, float> LeftHalf = new Dictionary<GameObject, float>();
+            for(int i = 0; i < midpoint; i++)
+            {
+                
+            }
+        }
+    }
+    
     private void Update()
     {
         PlayerBullets = GameObject.FindGameObjectsWithTag("Bullet");
@@ -19,12 +37,27 @@ public class EscapeEnemy : EnemyMovement
                 distance = Diff.magnitude;
             }
         }
+        
+        for(int i = 0; i < PlayerBullets.Length; i++)
+        {
+            GameObject NextBullet = PlayerBullets[i];
+            Vector2 Difference = NextBullet.GetComponent<Transform>().position - GetComponent<Transform>().position;
+            if (Difference.magnitude <= 8f)
+            {
+                ClosestBullets.Add(NextBullet, Difference.magnitude);
+            }
+        }
+        if(ClosestBullets.Count > 1)
+        {
+
+        }
     }
 
     private void FixedUpdate()
     {
         if(PlayerBullets.Length == 0)
         {
+            moveSpeed = 2.5f;
             if (player != null)
             {
                 transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
@@ -36,6 +69,10 @@ public class EscapeEnemy : EnemyMovement
                     Destroy(player);
                 }
             }
+        }
+        else
+        {
+            moveSpeed = 10f;
         }
     }
 }
